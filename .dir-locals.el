16e22755767cbf6a11aa6699d0679ff73618d216
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-10-15 01:25:24 +0100

    First commit!
    This is a from the ground rework of an old project of the same name.
    I'm hoping to be more concerned with runtime efficiency, bytecode size
    and all those things that should actually matter for something that
    may host time/space critical code.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((projectile-project-compilation-cmd . "make")
+         (projectile-project-configure-cmd   . "make clean")
+         (+license/license-choice            . "GPLv2"))))
