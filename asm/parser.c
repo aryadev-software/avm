131d70a9a37bdbfacaea9cfe2f78867dd9e16942
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-10-25 21:43:51 +0100

    Started working on a parser
    No implementations yet

---
+++ asm/parser.c
@@ -0,0 +1,11 @@
+/* Copyright (C) 2023 Aryadev Chavali
+
+ * You may distribute and modify this code under the terms of the
+ * GPLv2 license.  You should have received a copy of the GPLv2
+ * license with this file.  If not, please write to:
+ * aryadev@aryadevchavali.com.
+
+ * Created: 2023-10-24
+ * Author: Aryadev Chavali
+ * Description:
+ */
---
+++ asm/parser.h
@@ -0,0 +1,24 @@
+/* Copyright (C) 2023 Aryadev Chavali
+
+ * You may distribute and modify this code under the terms of the GPLv2
+ * license.  You should have received a copy of the GPLv2 license with
+ * this file.  If not, please write to: aryadev@aryadevchavali.com.
+
+ * Created: 2023-10-24
+ * Author: Aryadev Chavali
+ * Description: Parser for assembly language
+ */
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "./lexer.h"
+
+#include <vm/inst.h>
+
+typedef darr_t token_stream_t;
+
+inst_t parse_next_inst(token_stream_t *);
+inst_t *parse_stream(token_stream_t *, size_t *);
+
+#endif
