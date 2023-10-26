a07b571da2156bb0d151f20121a485ebfc62c0af
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-10-26 08:01:26 +0100

    Auto fill licenses

--- asm/parser.h
+++ asm/parser.h
@@ -1,8 +1,9 @@
 /* Copyright (C) 2023 Aryadev Chavali
 
- * You may distribute and modify this code under the terms of the GPLv2
- * license.  You should have received a copy of the GPLv2 license with
- * this file.  If not, please write to: aryadev@aryadevchavali.com.
+ * You may distribute and modify this code under the terms of the
+ * GPLv2 license.  You should have received a copy of the GPLv2
+ * license with this file.  If not, please write to:
+ * aryadev@aryadevchavali.com.
 
  * Created: 2023-10-24
  * Author: Aryadev Chavali
