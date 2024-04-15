58069e083df2b7ffb9917e110e3e2a45b67d1fcb
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-04-15 16:25:14 +0630

    Auto filled copyrightn otice in asm/base

--- asm/base.cpp
+++ asm/base.cpp
@@ -1,8 +1,9 @@
 /* Copyright (C) 2024 Aryadev Chavali
 
- * You may distribute and modify this code under the terms of the GPLv2
- * license.  You should have received a copy of the GPLv2 license with
- * this file.  If not, please write to: aryadev@aryadevchavali.com.
+ * You may distribute and modify this code under the terms of the
+ * GPLv2 license.  You should have received a copy of the GPLv2
+ * license with this file.  If not, please write to:
+ * aryadev@aryadevchavali.com.
 
  * Created: 2024-04-14
  * Author: Aryadev Chavali
