95a8747c7f3e7387666ae69f48ccddf651cc46f6
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-06-01 01:14:17 +0100

    Cleaned up .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil    . ((projectile-project-compilation-cmd . "make")
-            (projectile-project-configure-cmd   . "make clean")
-            (projectile-project-test-cmd        . "make test")
-            (+license/license-choice            . "GPLv2")))
+((nil    . ((+license/license-choice            . "GPLv2")))
  (c-mode . ((mode . clang-format)
             (eval . (eglot-ensure)))))
