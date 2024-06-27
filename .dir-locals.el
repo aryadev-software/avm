4c0c00183b8b03b75ae3b441bc3df851871a0f3a
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-06-28 00:27:32 +0100

    Add compile command value in dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil    . ((+license/license-choice            . "GPLv2")))
- (c-mode . ((mode . clang-format)
-            (eval . (eglot-ensure)))))
+((nil    . ((+license/license-choice . "GPLv2")
+            (compile-command         . "make all VERBOSE=2 RELEASE=0")))
+ (c-mode . ((mode                    . clang-format)
+            (eval                    . (eglot-ensure)))))
