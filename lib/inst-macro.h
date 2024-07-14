899c2689824a6ed10f1ab7a863c15000682d81d1
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-07-14 18:50:30 +0100

    Reimplement inst-macro
    Doesn't require macro magic to construct symbols on the fly, woo!

--- lib/inst-macro.h
+++ lib/inst-macro.h
@@ -16,59 +16,47 @@
 #ifndef INST_MACRO_H
 #define INST_MACRO_H
 
-#include "./inst.h"
-
 #define INST_NOOP ((inst_t){0})
-#define INST_HALT ((inst_t){.opcode = OP_HALT})
+#define INST_HALT ((inst_t){.opcode = OP_HALT, .n = 0, .operands = NULL})
 
-#define INST_PUSH(TYPE, OP) \
-  ((inst_t){.opcode = OP_PUSH_##TYPE, .operand = D##TYPE(OP)})
+#define INST_PUSH(N, OPS) ((inst_t){.opcode = OP_PUSH, .n = N, .operands = OPS})
+#define INST_PUSH_REGISTER(N, OPS) \
+  ((inst_t){.opcode = OP_PUSH_REGISTER, .n = N, .operands = OPS})
+#define INST_JUMP_IF(N, OPS) \
+  ((inst_t){.opcode = OP_JUMP_IF, .n = N, .operands = OPS})
 
-#define INST_MOV(TYPE, OP) \
-  ((inst_t){.opcode = OP_MOV_##TYPE, .operand = DWORD(OP)})
-
-#define INST_POP(TYPE) ((inst_t){.opcode = OP_POP_##TYPE})
-
-#define INST_PUSH_REG(TYPE, REG) \
-  ((inst_t){.opcode = OP_PUSH_REGISTER_##TYPE, .operand = D##TYPE(REG)})
-
-#define INST_DUP(TYPE, OP) \
-  ((inst_t){.opcode = OP_DUP_##TYPE, .operand = DWORD(OP)})
-
-#define INST_MALLOC(TYPE, OP) \
-  ((inst_t){.opcode = OP_MALLOC_##TYPE, .operand = DWORD(OP)})
-#define INST_MALLOC_STACK(TYPE) ((inst_t){.opcode = OP_MALLOC_STACK_##TYPE})
-#define INST_MSET(TYPE, OP) \
-  ((inst_t){.opcode = OP_MSET_##TYPE, .operand = DWORD(OP)})
-#define INST_MSET_STACK(TYPE) ((inst_t){.opcode = OP_MSET_STACK_##TYPE})
-#define INST_MGET(TYPE, OP) \
-  ((inst_t){.opcode = OP_MGET_##TYPE, .operand = DWORD(OP)})
-#define INST_MGET_STACK(TYPE) ((inst_t){.opcode = OP_MGET_STACK_##TYPE})
-#define INST_MDELETE          ((inst_t){.opcode = OP_MDELETE})
-#define INST_MSIZE            ((inst_t){.opcode = OP_MSIZE})
-
-#define INST_NOT(TYPE)  ((inst_t){.opcode = OP_NOT_##TYPE})
-#define INST_OR(TYPE)   ((inst_t){.opcode = OP_OR_##TYPE})
-#define INST_AND(TYPE)  ((inst_t){.opcode = OP_AND_##TYPE})
-#define INST_XOR(TYPE)  ((inst_t){.opcode = OP_XOR_##TYPE})
-#define INST_EQ(TYPE)   ((inst_t){.opcode = OP_EQ_##TYPE})
-#define INST_LT(TYPE)   ((inst_t){.opcode = OP_LT_##TYPE})
-#define INST_LTE(TYPE)  ((inst_t){.opcode = OP_LTE_##TYPE})
-#define INST_GT(TYPE)   ((inst_t){.opcode = OP_GT_##TYPE})
-#define INST_GTE(TYPE)  ((inst_t){.opcode = OP_GTE_##TYPE})
-#define INST_PLUS(TYPE) ((inst_t){.opcode = OP_PLUS_##TYPE})
-#define INST_SUB(TYPE)  ((inst_t){.opcode = OP_SUB_##TYPE})
-#define INST_MULT(TYPE) ((inst_t){.opcode = OP_MULT_##TYPE})
-
-#define INST_JUMP_ABS(OP) \
-  ((inst_t){.opcode = OP_JUMP_ABS, .operand = DWORD(OP)})
-#define INST_JUMP_STACK ((inst_t){.opcode = OP_JUMP_STACK})
-#define INST_JUMP_IF(TYPE, OP) \
-  ((inst_t){.opcode = OP_JUMP_IF_##TYPE, .operand = DWORD(OP)})
-#define INST_CALL(OP)   ((inst_t){.opcode = OP_CALL, .operand = DWORD(OP)})
-#define INST_CALL_STACK ((inst_t){.opcode = OP_CALL_STACK})
-#define INST_RET        ((inst_t){.opcode = OP_RET})
-
-#define INST_PRINT(TYPE) ((inst_t){.opcode = OP_PRINT_##TYPE})
+#define INST_MOV(N, OPS) ((inst_t){.opcode = OP_MOV, .n = N, .operands = OPS})
+#define INST_POP(N)      ((inst_t){.opcode = OP_POP, .n = N, .operands = NULL})
+#define INST_DUP(N)      ((inst_t){.opcode = OP_DUP, .n = N, .operands = NULL})
+#define INST_NOT(N)      ((inst_t){.opcode = OP_NOT, .n = N, .operands = NULL})
+#define INST_OR(N)       ((inst_t){.opcode = OP_OR, .n = N, .operands = NULL})
+#define INST_AND(N)      ((inst_t){.opcode = OP_AND, .n = N, .operands = NULL})
+#define INST_XOR(N)      ((inst_t){.opcode = OP_XOR, .n = N, .operands = NULL})
+#define INST_EQ(N)       ((inst_t){.opcode = OP_EQ, .n = N, .operands = NULL})
+#define INST_PLUS_UNSIGNED(N) \
+  ((inst_t){.opcode = OP_PLUS_UNSIGNED, .n = N, .operands = NULL})
+#define INST_MULT_UNSIGNED(N) \
+  ((inst_t){.opcode = OP_MULT_UNSIGNED, .n = N, .operands = NULL})
+#define INST_SUB_UNSIGNED(N) \
+  ((inst_t){.opcode = OP_SUB_UNSIGNED, .n = N, .operands = NULL})
+#define INST_PLUS(N) ((inst_t){.opcode = OP_PLUS, .n = N, .operands = NULL})
+#define INST_SUB(N)  ((inst_t){.opcode = OP_SUB, .n = N, .operands = NULL})
+#define INST_MULT(N) ((inst_t){.opcode = OP_MULT, .n = N, .operands = NULL})
+#define INST_LT_UNSIGNED(N) \
+  ((inst_t){.opcode = OP_LT_UNSIGNED, .n = N, .operands = NULL})
+#define INST_LTE_UNSIGNED(N) \
+  ((inst_t){.opcode = OP_LTE_UNSIGNED, .n = N, .operands = NULL})
+#define INST_GT_UNSIGNED(N) \
+  ((inst_t){.opcode = OP_GT_UNSIGNED, .n = N, .operands = NULL})
+#define INST_GTE_UNSIGNED(N) \
+  ((inst_t){.opcode = OP_GTE_UNSIGNED, .n = N, .operands = NULL})
+#define INST_LT(N)  ((inst_t){.opcode = OP_LT, .n = N, .operands = NULL})
+#define INST_LTE(N) ((inst_t){.opcode = OP_LTE, .n = N, .operands = NULL})
+#define INST_GT(N)  ((inst_t){.opcode = OP_GT, .n = N, .operands = NULL})
+#define INST_GTE(N) ((inst_t){.opcode = OP_GTE, .n = N, .operands = NULL})
+#define INST_JUMP_ABS(N) \
+  ((inst_t){.opcode = OP_JUMP_ABS, .n = N, .operands = NULL})
+#define INST_CALL(N) ((inst_t){.opcode = OP_CALL, .n = N, .operands = NULL})
+#define INST_RET(N)  ((inst_t){.opcode = OP_RET, .n = N, .operands = NULL})
 
 #endif
