e983ec19aa451eaa1c6a38b47f7e782177d3a79d
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-07-14 16:23:41 +0100

    Implement some macros to check arity of an opcode
    Since instructions can potentially take multiple arguments, it's good
    to a strict arity check.

--- lib/inst.h
+++ lib/inst.h
@@ -67,4 +67,21 @@ typedef enum
   NUMBER_OF_OPCODES,
 } opcode_t;
 
+#define IS_OPCODE_NULLARY(OP) \
+  ((OP) == OP_NOOP || (OP) == OP_HALT || (OP) == OP_RET)
+
+#define IS_OPCODE_UNARY(OP)                                                    \
+  ((OP) == OP_POP || (OP) == OP_PUSH_REGISTER || (OP) == OP_DUP ||             \
+   (OP) == OP_NOT || (OP) == OP_OR || (OP) == OP_AND || (OP) == OP_XOR ||      \
+   (OP) == OP_EQ || (OP) == OP_PLUS_UNSIGNED || (OP) == OP_MULT_UNSIGNED ||    \
+   (OP) == OP_SUB_UNSIGNED || (OP) == OP_PLUS || (OP) == OP_SUB ||             \
+   (OP) == OP_MULT || (OP) == OP_LT_UNSIGNED || (OP) == OP_LTE_UNSIGNED ||     \
+   (OP) == OP_GT_UNSIGNED || (OP) == OP_GTE_UNSIGNED || (OP) == OP_LT ||       \
+   (OP) == OP_LTE || (OP) == OP_GT || (OP) == OP_GTE || (OP) == OP_JUMP_ABS || \
+   (OP) == OP_CALL)
+
+#define IS_OPCODE_BINARY(OP) ((OP) == OP_MOV || (OP) == OP_JUMP_IF)
+
+#define IS_OPCODE_NARY(OP) ((OP) == OP_PUSH)
+
 #endif
